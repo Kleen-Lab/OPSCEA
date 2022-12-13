e78024ff3a5912dceae5db72f5871c0225f43435
Raphaël Christin <raphael.christin@mail.mcgill.ca>
2022-12-12 17:02:07 -0800

    Revert splitbrain to before #5 Splitbrain as proposed in #4 caused the issue outlined in #10 We are reverting to avoid the issue and will continue development in another branch. The faulty version of splitbrain is retained for testing.

--- test_OPSCEA.m
+++ test_OPSCEA.m
@@ -2,7 +2,7 @@ function test_OPSCEA()
     patient_list = readtable("OPSCEAparams.xlsx", "Sheet", "params", 'DataRange', 'A2:A240');
     seizure_list = readtable("OPSCEAparams.xlsx", "Sheet", "params", "DataRange", "B2:B240");
     
-    for i=40:height(patient_list)
+    for i=35:height(patient_list)
         pt = char(patient_list{i,1});
         sz = char(seizure_list{i,1});
         if strcmp(pt, 'TD143_singlebrain');continue;end;
