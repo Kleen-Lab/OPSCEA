065b758d575945b94a9021b6843d0cd9ee9572c2
Raphaël Christin <raphael.christin@mail.mcgill.ca>
2022-12-13 15:22:54 -0800

    Close figures when finishing the video Avoid all figures being still on the screen

--- test_OPSCEA.m
+++ test_OPSCEA.m
@@ -2,10 +2,11 @@ function test_OPSCEA()
     patient_list = readtable("OPSCEAparams.xlsx", "Sheet", "params", 'DataRange', 'A2:A240');
     seizure_list = readtable("OPSCEAparams.xlsx", "Sheet", "params", "DataRange", "B2:B240");
     
-    for i=35:height(patient_list)
+    for i=130:height(patient_list)
         pt = char(patient_list{i,1});
         sz = char(seizure_list{i,1});
         if strcmp(pt, 'TD143_singlebrain');continue;end;
         OPSCEA(pt, sz, 1, 0)
+        close all;
     end
 end
