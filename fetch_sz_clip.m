3c80cc4b3d2d8c143b6479272918b84bee0e6d6c
Raphaël Christin <70120121+raphaelchristin@users.noreply.github.com>
2024-05-17 16:14:35 -0700

    Add fetch_sz_clip
    Uses python and anthony's pipeline to fetch seizure clip instead of the
    previous .mat files. not yet integrated in the broader codebase

---
+++ fetch_sz_clip.m
@@ -0,0 +1,22 @@
+function [d, sfx] = fetch_sz_clip(pt_id,start_dt,end_dt,python_path,py_module_path)
+    arguments
+        pt_id (1,:) char;
+        start_dt (1,19) char;
+        end_dt (1,19) char;
+        python_path (1,:) char = '/scratch/fetch_clip/.venv/bin/python';
+        py_module_path (1, :) char = '/scratch/fetch_clip';
+    end
+
+    %setup python
+    pyenv(Version=python_path);
+    insert(py.sys.path, int64(0), py_module_path);
+    
+    %fetch the clip
+    pystruct = py.opscea_fetch.fetch_clip(pt_id, start_dt, end_dt);
+    
+    %unpack and convert python data to matlab objects
+    py_d = pystruct{1};
+    py_sfx = pystruct{2};
+    d = double(py_d);
+    sfx = double(py_sfx);
+end
